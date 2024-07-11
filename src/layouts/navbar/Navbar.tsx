3a8a516915558d022b6172d2dc4022632cb2872f
Adrian Joltea <jolteaadrian@gmail.com>
2024-07-11 21:13:53 +0300

    KAN-5 | Create the navbar

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import styles from "./navbar.module.scss";
+import Logo from "@assets/brand/sipnplay.svg?react";
+
+const NAV_LINKS = [
+  { text: "About", to: "about" },
+  { text: "Menu", to: "menu" },
+  { text: "Board Games", to: "board-games" },
+  {
+    text: "Calendar",
+    to: "calendar",
+  },
+];
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles["header-container"]}>
+      <header className={styles["header"]}>
+        <Logo />
+        <div>
+          <nav className={`${styles["nav"]}`}>
+            <div className={styles["nav-content"]}>
+              <div className={styles["nav-buttons"]}>
+                {NAV_LINKS.map((btn, i) => (
+                  <button className="button-link" key={i}>
+                    {btn.text}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Navbar;
