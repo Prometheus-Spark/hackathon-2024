c733ca36fa7ae49565a0f6ffaaba3574f3375538
Adrian Joltea <jolteaadrian@gmail.com>
2024-07-11 20:02:16 +0300

    KAN-1 | Create the folder structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: { host: true },
+  resolve: {
+    alias: {
+      "@root": "/src",
+      "@features": "/src/features",
+      "@assets": "/src/assets",
+      "@hooks": "/src/hooks",
+    },
+  },
+});
