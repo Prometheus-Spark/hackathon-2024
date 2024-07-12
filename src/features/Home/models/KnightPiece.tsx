6a655d548fa3273841f8245670796ce920841715
Adrian Joltea <jolteaadrian@gmail.com>
2024-07-12 15:44:35 +0300

    KAN-34 | Final changes

--- src/features/Home/models/KnightPiece.tsx
+++ src/features/Home/models/KnightPiece.tsx
@@ -7,32 +7,40 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 import Knight from "../assets/KnightPiece.glb";
+import { useFrame } from "@react-three/fiber";
 
 type GLTFResult = GLTF & {
-	nodes: {
-		Cube: THREE.Mesh;
-	};
-	materials: {
-		Black: THREE.MeshStandardMaterial;
-	};
+  nodes: {
+    Cube: THREE.Mesh;
+  };
+  materials: {
+    Black: THREE.MeshStandardMaterial;
+  };
 };
 
 export function KnightPiece(props: JSX.IntrinsicElements["group"]) {
-	const { nodes, materials } = useGLTF(Knight) as GLTFResult;
-	return (
-		<group
-			{...props}
-			dispose={null}>
-			<mesh
-				castShadow
-				receiveShadow
-				geometry={nodes.Cube.geometry}
-				material={materials.Black}
-				position={[3.984, 0.059, -0.035]}
-				scale={0.314}
-			/>
-		</group>
-	);
+  const { nodes, materials } = useGLTF(Knight) as GLTFResult;
+  const groupRef = useRef<THREE.Group>(null);
+
+  useFrame(({ clock }) => {
+    if (groupRef.current) {
+      groupRef.current.position.y = Math.sin(clock.getElapsedTime()) * 0.05;
+      groupRef.current.rotation.x = Math.sin(clock.getElapsedTime()) * 0.05;
+    }
+  });
+
+  return (
+    <group ref={groupRef} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={materials.Black}
+        position={[3.984, 0.059, -0.035]}
+        scale={0.314}
+      />
+    </group>
+  );
 }
 
 useGLTF.preload(Knight);
--- src/features/Home/models/RookPiece.tsx
+++ src/features/Home/models/RookPiece.tsx
@@ -3,31 +3,39 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 import Rook from "../assets/RookPiece.glb";
+import { useFrame } from "@react-three/fiber";
 
 type GLTFResult = GLTF & {
-	nodes: {
-		Circle002: THREE.Mesh;
-	};
-	materials: {
-		White: THREE.MeshStandardMaterial;
-	};
+  nodes: {
+    Circle002: THREE.Mesh;
+  };
+  materials: {
+    White: THREE.MeshStandardMaterial;
+  };
 };
 
 export function RookPiece(props: JSX.IntrinsicElements["group"]) {
-	const { nodes, materials } = useGLTF(Rook) as GLTFResult;
-	return (
-		<group
-			{...props}
-			dispose={null}>
-			<mesh
-				castShadow
-				receiveShadow
-				geometry={nodes.Circle002.geometry}
-				material={materials.White}
-				position={[-1, 0, -0.035]}
-			/>
-		</group>
-	);
+  const { nodes, materials } = useGLTF(Rook) as GLTFResult;
+  const groupRef = useRef<THREE.Group>(null);
+
+  useFrame(({ clock }) => {
+    if (groupRef.current) {
+      groupRef.current.position.y = Math.sin(clock.getElapsedTime()) * 0.05;
+      groupRef.current.rotation.x = Math.sin(clock.getElapsedTime()) * 0.05;
+    }
+  });
+
+  return (
+    <group ref={groupRef} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Circle002.geometry}
+        material={materials.White}
+        position={[-1, 0, -0.035]}
+      />
+    </group>
+  );
 }
 
 useGLTF.preload(Rook);
